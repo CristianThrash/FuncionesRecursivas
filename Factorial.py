e677eb1dcaa622bf54cf6c2de95357738c57c146
Cristian Thrash <cristianthrashx@gmail.com>
2017-02-25 20:05:02 -0500

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -7,7 +7,7 @@ Created on 18/02/2017
 '''Retorna el factorial de un numero dado.'''
 def factorial(numero):
     if(numero==0):
-        return 1;
+        return 1
     else:
         return numero*factorial(numero-1)
     
